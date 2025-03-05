46d355a668433cf34491a82102e161b958eada9d
Roshan Chand <rosanchand1234@gmail.com>
2025-03-05 11:23:38 +0545

    phonebookuibuild

--- index.js
+++ index.js
@@ -140,7 +140,12 @@ app.put('/api/persons/:id',(request,response,next)=>{
     name: body.name,
     number: body.number,
   }
-  Person.findByIdAndUpdate(request.params.id,person,{new:true})
+  Person.findByIdAndUpdate(request.params.id,person,
+    {
+     new:true,
+     runValidators: true,
+     context: 'query' 
+    })
   .then(updatedPerson => {
     response.json(updatedPerson)
   })
@@ -148,7 +153,7 @@ app.put('/api/persons/:id',(request,response,next)=>{
 
 })
 
-app.post('/api/persons',(request,response)=>{
+app.post('/api/persons',(request,response,next)=>{
   const body = request.body
   if(!body.name){
     return response.status(400).json({
@@ -180,6 +185,8 @@ app.post('/api/persons',(request,response)=>{
     })
     personInfo.save().then(savedPerson =>{
       response.json(savedPerson)
+    }).catch(error => {
+      next(error)
     })
 
 })
@@ -195,6 +202,8 @@ const errorHandler = (error ,request,response,next) => {
   if(error.name === 'CastError'){
     return response.status(400).send({error:'malformatted id'})
 
+  } else if (error.name === 'ValidationError'){
+    return response.status(400).json({error: error.message})
   }
   next(error)
 }
--- models/Person.js
+++ models/Person.js
@@ -8,8 +8,22 @@ mongoose.set('strictQuery',false)
 mongoose.connect(uri)
 
 const phonebookSchema = new mongoose.Schema({
-    name: String,
-    number: String,
+    name: {
+        type: String,
+        required: true,
+        minLength: 3,
+    },
+    number: {
+        type: String,
+        minLength: 8,
+        validate: {
+            validator: function(v){
+            return /^\d{2,3}-\d{0,9}/.test(v);
+        },
+        message: props=> `${props.value} is not a valid phone number`
+    },
+    required: true,
+}
 })
 phonebookSchema.set('toJSON',{
     transform:(document,returnedObject) =>{
