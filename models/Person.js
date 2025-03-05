f9effd581c87ab40a61f03539078a029374cf93f
Roshan Chand <rosanchand1234@gmail.com>
2025-03-05 11:26:06 +0545

    ex: 3.19* - 3.21

--- models/Person.js
+++ models/Person.js
@@ -20,7 +20,7 @@ const phonebookSchema = new mongoose.Schema({
             validator: function(v){
             return /^\d{2,3}-\d{0,9}/.test(v);
         },
-        message: props=> `${props.value} is not a valid phone number`
+        message: props=> `${props.value} is not a valid phone number.format: {NN or NNN}-NNN..`
     },
     required: true,
 }
